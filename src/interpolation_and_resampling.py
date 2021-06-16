688dacdcd6c1a96894e0ca05c6f4540ef78e232c
rubieywh <rubieywh@aliyun.com>
2021-06-16 15:03:54 +0900

    finished

--- src/interpolation_and_resampling.py
+++ src/interpolation_and_resampling.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 from util.preproccessing import curve_handler
 
@@ -12,14 +13,24 @@ with open('../source_data/raw.csv', 'r', encoding='utf-8-sig') as raw:
     raw_data = rows
     raw.close()
 
-processed_curve = {}
+valid_number_list = []
+valid_label_list =[]
+
+processed_curve_container = {}
+urination_time_container = {}
+q_max_container = {}
+u_total_container = {}
+
+
+resampling_frequency = 400
 
 for data in raw_data:
     number = data[0]
+    label = data[12]
     f_name = '../source_data/plot_data/' + str(number) + '.csv'
     if os.path.exists(f_name) & os.path.isfile(f_name):
         print(f_name + ' exists')
-        curve_raw_data = np.loadtxt(f_name, delimiter=',', skiprows=0).T
+        curve_raw_data = np.loadtxt(f_name, encoding='utf-8-sig', delimiter=',', skiprows=0).T
         if curve_raw_data.shape[0] != 2:
             print('dimension error')
             break
@@ -27,8 +38,42 @@ for data in raw_data:
             curve_raw_data_x = curve_raw_data[0]
             curve_raw_data_y = curve_raw_data[1]
 
-            curve_handler(curve_raw_data_x, curve_raw_data_y)
+            processed_curve_container[number], urination_time_container[number], \
+                q_max_container[number], u_total_container[number] = \
+                curve_handler(curve_raw_data_x, curve_raw_data_y, resampling_frequency)
+
+            valid_number_list.append(number)
+            valid_label_list.append(label)
     else:
         print(f_name + ' not exists')
 
-    break
+with open('../output/curves.csv', 'w') as curve_datafile:
+    csv_writer = csv.writer(curve_datafile)
+    for valid_number in valid_number_list:
+        csv_writer.writerow(processed_curve_container[valid_number])
+
+        # save plots into image file
+        x = range(resampling_frequency)
+        y = processed_curve_container[valid_number]
+        plt.plot(x, y, color='black', linewidth=1)
+        plt.savefig('../output/figs/' + valid_number + '.jpg')
+        plt.show()
+
+    curve_datafile.close()
+
+with open('../output/label.txt', 'w') as label_datafile:
+    for valid_label in valid_label_list:
+        label_datafile.write(valid_label + '\n')
+    label_datafile.close()
+
+with open('../output/info.csv', 'w') as info_datafile:
+    csv_writer = csv.writer(info_datafile)
+    for valid_number in valid_number_list:
+        info = [
+                valid_number,
+                urination_time_container[valid_number],
+                q_max_container[valid_number],
+                u_total_container[valid_number]
+        ]
+        csv_writer.writerow(info)
+    info_datafile.close()
--- src/util/preproccessing.py
+++ src/util/preproccessing.py
@@ -1,5 +1,28 @@
 import numpy as np
 
-def curve_handler(raw_x, raw_y):
-    pass
-    print(raw_x, raw_y)
+
+def curve_handler(raw_x, raw_y, frequency):
+    start = raw_x[0]
+    end = raw_x[-1]
+
+    # get linear interpolation and 400-resampling done on irregular series
+    resampling_points_x = np.linspace(start, end, frequency)
+    resampling_points_y = np.interp(resampling_points_x, raw_x, raw_y)
+
+    # get q_max representing maximum instantaneous flow
+    q_max = np.max(resampling_points_y)
+    normalized_y = resampling_points_y / q_max
+
+    # get u_total representing total urine output via calculus
+    time_interval = (end - start) / float(frequency - 1)
+    u_total = u_total_calculus(time_interval, resampling_points_y)
+
+    return normalized_y, end, q_max, u_total
+
+
+def u_total_calculus(time_interval, y):
+    u_total = 0
+    for i in range(len(y) - 1):
+        average = (y[i] + y[i+1]) / 2.
+        u_total += average * time_interval
+    return u_total
